b83927cfad2ea203874caf106ff16155af9c27bb
Andres Trujillo <AndresSphere2003@gmail.com>
2024-01-26 21:19:22 -0800

    Changes to MainMenu ~ Andres

--- HorseSoccer/UI/MainMenu.gd
+++ HorseSoccer/UI/MainMenu.gd
@@ -1,5 +1,4 @@
-extends Node
-
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +8,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/Test3DLevel.tscn")
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
