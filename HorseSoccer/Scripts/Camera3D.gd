317484eae612ade2277e5dda7b136ad20e86bd77
Andres Trujillo <AndresSphere2003@gmail.com>
2024-01-27 14:54:17 -0800

    Added pause menu support
    + fixes to UI scene switches

--- HorseSoccer/Scripts/Camera3D.gd
+++ HorseSoccer/Scripts/Camera3D.gd
@@ -1,9 +1,19 @@
 extends Camera3D
 
+# Preload the pause menu scene
+var PauseMenuScene = preload("res://UI/PauseMenu.tscn")
+var pause_menu = PauseMenuScene.instantiate()
+
+var paused = false
+
 @export var target: Node3D
 var targetPos
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# pause menu init
+	add_child(pause_menu)
+	pause_menu.visible = false
+	
 	targetPos = Vector3(0,0,0)
 	pass # Replace with function body.
 
@@ -11,8 +21,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
+	if Input.is_action_just_pressed("pauseGame"):
+		paused = true
+		pauseGame()
+	
 	var lookat_pos = target.global_position
 	look_at(lookat_pos+Vector3(0,2,0))
 	targetPos = target.get_node("Camera").global_position+Vector3(0,5,0);
 	self.position = self.position.lerp(targetPos, delta*3.0)
 	pass
+
+# pause game and display pause menu
+func pauseGame():
+	if paused:
+		pause_menu.visible = true
+		get_tree().paused = true
+		paused = !paused
+		
