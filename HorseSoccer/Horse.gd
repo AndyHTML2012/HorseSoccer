197305555de3dfb2a8d8b9daf184fe3288161cdf
Rorybabory <lambertrory123@gmail.com>
2024-01-27 11:48:05 -0800

    horse physics

--- HorseSoccer/Horse.gd
+++ HorseSoccer/Horse.gd
@@ -1,42 +1,59 @@
-extends CharacterBody3D
+extends RigidBody3D
 
 
-const SPEED = 0.6
+const SPEED = 20.0
 const TURN_SPEED = 4.0
 const JUMP_VELOCITY = 10
 const MAX_VELOCITY = 10.0
 
 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mousedelta = 0
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mousedelta = event.relative.x * 0.02
 
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+#	if not is_on_floor():
+#		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "forward", "backward")
 	
 	var movement = input_dir.y
 	if movement:
-		velocity.x += movement * SPEED * transform.basis.z.x
-		velocity.z += movement * SPEED * transform.basis.z.z
+		constant_force.x = movement * SPEED * transform.basis.z.x
+		constant_force.z = movement * SPEED * transform.basis.z.z
 	else:
-		velocity.x = move_toward(velocity.x, 0, delta*10.0)
-		velocity.z = move_toward(velocity.z, 0, delta*10.0)
+		constant_force.x = move_toward(constant_force.x, 0, delta*10.0)
+		constant_force.z = move_toward(constant_force.z, 0, delta*10.0)
 
-	velocity.x = clamp(velocity.x, -MAX_VELOCITY, MAX_VELOCITY)
-	velocity.z = clamp(velocity.z, -MAX_VELOCITY, MAX_VELOCITY)
+	#constant_force.x = clamp(constant_force.x, -MAX_VELOCITY, MAX_VELOCITY)
+	#constant_force.z = clamp(constant_force.z, -MAX_VELOCITY, MAX_VELOCITY)
 	print(str(transform.basis))
-	var rot_value = -input_dir.x*TURN_SPEED*delta;
+	if (Input.is_action_just_pressed("ui_cancel")):
+		if (Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
+	
+	var rot_value = -mousedelta*TURN_SPEED*delta;
 	rotation_degrees.z += rot_value * 30.0;
-	rotation_degrees.z = move_toward(rotation_degrees.z, 0, delta*70.0)
+	rotation_degrees.z = move_toward(rotation_degrees.z, 0, delta*150.0)
+	rotation_degrees.x = move_toward(rotation_degrees.x, 0, delta*150.0)
 	rotate_y(rot_value);
-	move_and_slide()
+
+	
+	#physics push:
+
--- HorseSoccer/Scripts/Camera3D.gd
+++ HorseSoccer/Scripts/Camera3D.gd
@@ -1,12 +1,18 @@
 extends Camera3D
 
-
+@export var target: Node3D
+var targetPos
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	targetPos = Vector3(0,0,0)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_rotation.z = 0;
+	
+	var lookat_pos = target.global_position
+	look_at(lookat_pos+Vector3(0,2,0))
+	targetPos = target.get_node("Camera").global_position+Vector3(0,5,0);
+	self.position = self.position.lerp(targetPos, delta*3.0)
 	pass
