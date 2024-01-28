fd514f7110151c4497b66662dcb75aa081c0b178
Andres Trujillo <AndresSphere2003@gmail.com>
2024-01-28 14:51:21 -0800

    end screen stat changes

--- HorseSoccer/UI/GameLose.gd
+++ HorseSoccer/UI/GameLose.gd
@@ -1,4 +1,13 @@
 extends Control
+var horse = null
+
+func _ready():
+	var root = get_tree().get_root()
+	horse = root.get_node("Horse")
+	if horse == null:
+		$VBoxContainer/BallHitsLabel.text = "Ball Hits: n/a"
+	else:
+		$VBoxContainer/BallHitsLabel.text = "Ball Hits:" + horse.num_ball_hits
 
 func _on_restart_button_pressed():
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
