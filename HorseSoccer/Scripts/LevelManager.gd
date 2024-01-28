ff65db5de960acb27e0c4ad2c7c7a00602800c24
Andres Trujillo <AndresSphere2003@gmail.com>
2024-01-28 13:40:09 -0800

    working loose screen

--- HorseSoccer/Scripts/LevelManager.gd
+++ HorseSoccer/Scripts/LevelManager.gd
@@ -10,10 +10,18 @@ signal Death
 var WinMenuScene = preload("res://UI/GameWin.tscn")
 var winnerScreen = WinMenuScene.instantiate()
 
+# Preload the loser menu scene
+var LoseMenuScene = preload("res://UI/GameLose.tscn")
+var looserScreen = LoseMenuScene.instantiate()
+
 func _ready():
 	# win menu init
 	add_child(winnerScreen)
 	winnerScreen.visible = false
+	
+	# lose menu init
+	add_child(looserScreen)
+	looserScreen.visible = false
 	
 	if (get_parent().has_node("netmodel")):
 		Goalie = get_parent().get_node("netmodel")
@@ -39,14 +47,15 @@ func _on_death_pit_body_entered(body):
 		emit_signal("Death")
 
 func Dying():
-	$UIFadeIn.play("FadeInLoser")
-	Failure.play()
-	var RestartTimer = Timer.new()
-	add_child(RestartTimer)
-	RestartTimer.wait_time = 5.5
-	RestartTimer.one_shot = true
-	RestartTimer.timeout.connect(Restart)
-	RestartTimer.start()
+	#$UIFadeIn.play("FadeInLoser")
+	Show_GameLoseScreen()
+	#Failure.play()
+	#var RestartTimer = Timer.new()
+	#add_child(RestartTimer)
+	#RestartTimer.wait_time = 5.5
+	#RestartTimer.one_shot = true
+	#RestartTimer.timeout.connect(Restart)
+	#RestartTimer.start()
 
 func _input(event):
 	if Input.is_key_pressed(KEY_R):
@@ -60,4 +69,17 @@ func Show_GameWinScreen():
 	
 
 func Show_GameLoseScreen():
-	pass
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	looserScreen.visible = true
+	Failure.play()
+	var RestartTimer = Timer.new()
+	add_child(RestartTimer)
+	RestartTimer.wait_time = 5.5
+	RestartTimer.one_shot = true
+	RestartTimer.timeout.connect(Restart)
+	RestartTimer.start()
+	
+	if RestartTimer.time_left <= 0:
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+		looserScreen.visible = false
+		Restart()
